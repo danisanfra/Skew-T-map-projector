029dd84443e25553cd60fc69b839a79224a21939
danisanfra <32964030+danisanfra@users.noreply.github.com>
2017-10-22 15:12:19 +0200

    Add files via upload

--- path_variables.py
+++ path_variables.py
@@ -1,4 +1,4 @@
-maps_folder       = ''
+maps_folder = 'C:/Users/Daniele/Desktop/UNIVERSITA/Atmosfera/maps/'
 stationloc_folder = maps_folder + 'stations_loc.txt'
 soundings_folder  = maps_folder + 'soundings\\'
 url_folder        = 'file:///' + maps_folder + 'html\\'
