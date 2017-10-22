85b6afe55799c240707f0f5cfabe02939592d8c2
danisanfra <32964030+danisanfra@users.noreply.github.com>
2017-10-22 12:23:43 +0200

    Add files via upload

--- skew-T.py
+++ skew-T.py
@@ -1,46 +1,22 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-
 from mpl_toolkits.basemap import Basemap
-"""
-Skew-T map projector - v0.1
-
-This program reads the latitude and ID number of stations from which soundings are usually available. They are reported in file stations_loc.txt. 
-
-Variables 'year', 'month', 'day' and 'hour' are set for the soundings.
-folder_path variable indicates the folder in which html pages are saved and from which the program converts the data to txt before reading them. At the moment data download is manual, planning to make it automatic in the next future. Path should be changed until the 'maps' folder, which contains this script.
-
-Next 'for' iteration reads inside station_ID array, to open the 'url' html file, convert it to .txt format and save it inside a file located in the 'soundings' subfolder. .txt formatted data are archived by date and time.
-Nested 'for' cycle (around line 60) comments with a '#' lines for which not all data are available in the columns. This isn't a very clever idea, I admit, but running the program all at once, I couldn't find anything more useful to get rid of extraction errors.
-
-Next 'for' cycle opens the txt soundings' files and saves data into multidimensional arrays (being at the moment just for geopotential height at 500 hPa).
-
-Last lines plot on a mercator projected map of Europe dots representing the location of stations and altitude of reading with the associated label.
-
-TO DO LIST:
-    - automatic data mining from wyoming university website (or other indication). This requires an automatic check for unavailable soundings that does not end in script's error stop.
-    - color gradient data representation for heights, temperatures humidity and other scalar quantities
-    - vector representation of vectorial quantities such as wind, pressure gradients or temperature gradients. This obviously requires a defined function for extracting gradients in a useful way.
-    - create installer to check for missing packages and to install them
-    - define different functions and (eventually) create a library to decide which type of data display or to display all data at once on different/same plot.
-"""
-
 import matplotlib.pyplot as plt
 import numpy
 import pylab
-
 import os
 
+from path_variables import *
 plt.close()
 
-station_lat, station_lon, station_ID = pylab.loadtxt('C:\\Users\\Daniele\\Desktop\\UNIVERSITA\'\\Atmosfera\\maps\\stations_loc.txt', unpack=True)
+station_lat, station_lon, station_ID = pylab.loadtxt(stationloc_folder, unpack=True)
 
 year  = '17'
 month = '10'
 day   = '09'
 hour  = '00' #either 12 or 00
 
-folder_path = 'C:\\Users\\Daniele\\Desktop\\UNIVERSITA\'\\Atmosfera\\maps\\soundings\\'+str(day)+str(month)+str(year)+'-'+str(hour)+'\\'
+folder_path = soundings_folder + str(day)+str(month)+str(year)+'-'+str(hour)+'\\'
 if not os.path.exists(folder_path):
     os.makedirs(folder_path)
 
@@ -49,7 +25,7 @@ for stn in station_ID:
         stn = '0' + str(int(stn))
     else:
         stn = str(int(stn))
-    url = 'file:///C:\\Users\\Daniele\\Desktop\\UNIVERSITA\'\\Atmosfera\maps\\html\\' + stn + '-' + day + month + year + '-' + hour + '.html'
+    url = url_folder + stn + '-' + day + month + year + '-' + hour + '.html'
 
 ## 1) Wyoming URL to download Sounding from / from already downloaded html file (above is path)
 # url = 'http://weather.uwyo.edu/cgi-bin/sounding?region=naconf&TYPE=TEXT%3ALIST&YEAR='+year+'&MONTH='+month+'&FROM='+day+hour+'&TO='+day+hour+'&STNM='+stn
